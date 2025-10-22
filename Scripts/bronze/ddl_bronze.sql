c259f13d40fb768497f7a5129f0a09bb24d75397
drxcy <156801775+drxcy@users.noreply.github.com>
2025-10-22 17:51:37 +0530

    Create 'transaksi' table in bronze schema
    This script creates the 'transaksi' table in the 'bronze' schema, dropping it if it already exists.

---
+++ Scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,22 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+IF OBJECT_ID('bronze.transaksi','U') IS NOT NULL
+	DROP TABLE bronze.transaksi
+
+GO
+CREATE TABLE bronze.transaksi(
+transaction_id INT,
+account_no NVARCHAR(50),
+tgl_transaksi DATE,
+amount INT,
+channel NVARCHAR(50),
+branch_code NVARCHAR(50),
+)
